d31f90891783af5edf378bcedb71fc1d77558b60
Davi Alves <cadwvk@gmail.com>
2023-03-13 17:11:33 -0300

    updated FerramentasDeDetalhe

--- src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
+++ src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
@@ -83,7 +83,7 @@ export const FerramentasDeDetalhe: React.FC<IFerramentasDeDetalheProps> = ({
         variant="outlined"
         startIcon={<Icon>save</Icon>}>
         <Typography variant="button" whiteSpace='nowrap' textOverflow='ellipsis' overflow='hidden'>
-          Salvar e Voltar
+          Salvar e fechar
         </Typography>
       </Button>)}
 
