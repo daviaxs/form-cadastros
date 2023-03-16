dd68a88bc77b203958b511a3843e9363a3c20776
Davi Alves <cadwvk@gmail.com>
2023-03-16 16:36:51 -0300

    created useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo } from "react"
 import { useSearchParams } from "react-router-dom"
 
 import { FerramentasDaListagem } from "../../shared/components"
+import { useDebounce } from "../../shared/hooks"
 import { LayoutBaseDePagina } from "../../shared/layouts"
 import { PessoasService } from "../../shared/services/api/pessoas/PessoasService"
 
@@ -9,6 +10,7 @@ import { PessoasService } from "../../shared/services/api/pessoas/PessoasService
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
+  const { debounce } = useDebounce(3000)
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || ''
@@ -16,15 +18,18 @@ export const ListagemDePessoas: React.FC = () => {
 
 
   useEffect(() => {
-    PessoasService.getAll(1, busca)
-      .then((result) => {
-        if (result instanceof Error) {
-          alert(result.message)
-        } else {
-          console.log(result)
-        }
-      })
-  }, [busca])
+
+    debounce(() => {
+      PessoasService.getAll(1, busca)
+        .then((result) => {
+          if (result instanceof Error) {
+            alert(result.message)
+          } else {
+            console.log(result)
+          }
+        })
+    })
+  }, [busca, debounce])
 
   return (
     <LayoutBaseDePagina
