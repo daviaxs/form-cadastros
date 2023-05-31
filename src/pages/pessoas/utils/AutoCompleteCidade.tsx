41ab7d1267a7a2e3bc57537e0f5e95db880b6651
Davi Alves <cadwvk@gmail.com>
2023-05-31 16:31:31 -0300

    Created useEffect in auto complete cidade

--- src/pages/pessoas/utils/AutoCompleteCidade.tsx
+++ src/pages/pessoas/utils/AutoCompleteCidade.tsx
@@ -1,4 +1,7 @@
 import { Autocomplete, TextField } from "@mui/material"
+import { useEffect, useState } from "react"
+import { useDebounce } from "../../../shared/hooks"
+import { CidadesService } from "../../../shared/services/api/cidades/CidadesService"
 
 type TAutoCompleteOption = {
   id: number
@@ -6,9 +9,32 @@ type TAutoCompleteOption = {
 }
 
 export const AutoCompleteCidade: React.FC = () => {
+  const { debounce } = useDebounce()
+  const [options, setOptions] = useState<TAutoCompleteOption[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+
+    setIsLoading(true)
+
+    debounce(() => {
+      CidadesService.getAll(1/*, busca*/)
+        .then((result) => {
+          setIsLoading(false)
+
+          if (result instanceof Error) {
+            alert(result.message)
+          } else {
+            setOptions(result.data.map(cidade => ({ id: cidade.id, label: cidade.nome })))
+          }
+
+        })
+    })
+  }, [debounce])
+
   return (
     <Autocomplete
-      options={[]}
+      options={[options]}
       renderInput={(params) => (
         <TextField
           {...params}
