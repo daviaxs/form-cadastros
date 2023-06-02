1136c18836402bfb1f0c69d7676e86dc63d69133
Davi Alves <cadwvk@gmail.com>
2023-06-02 17:47:03 -0300

    Added grids in dashboard page

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { Box, Grid } from "@mui/material"
 import { FerramentasDaListagem } from "../../shared/components"
 import { LayoutBaseDePagina } from "../../shared/layouts"
 
@@ -9,7 +10,15 @@ export const Dashboard = () => {
         <FerramentasDaListagem mostrarBotaoNovo={false} />
       )}
     >
+      <Box>
+        <Grid container>
+          <Grid item container>
+            <Grid item xs={12} md={6} lg={4} xl={2}>
 
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
     </LayoutBaseDePagina>
   )
 }
